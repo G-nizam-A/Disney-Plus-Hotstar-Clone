431c321f3f514e4cb81a5d06c7722a356a6ae1f6
G-nizam-A <109983860+G-nizam-A@users.noreply.github.com>
2023-08-07 17:48:36 +0530

    Initial commit

--- src/services/GlobalApi.js
+++ src/services/GlobalApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
                       
 const movieBaseUrl = "https://api.themoviedb.org/3"
-const api_key = '1293b879f03edfa88656fbea2abc4c39'
-const movieGenre = 'https://api.themoviedb.org/3/discover/movie?api_key=1293b879f03edfa88656fbea2abc4c39'
+const api_key = 'your-api-key'
+const movieGenre = 'https://api.themoviedb.org/3/discover/movie?api_key=your-api-key'
 
 const getTrending = axios.get(movieBaseUrl+"/trending/all/day?api_key="+api_key)
 const getMovieGenre=(id)=>axios.get(movieGenre+"&with_genres="+id)
